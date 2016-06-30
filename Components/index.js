ba6507e323926410995f2acfc92e79e69bc291cd
Mario Lo <kinfonglo@overstock.com>
2016-06-30 14:07:34 -0600

    Added a general accordions branch

--- Components/index.js
+++ Components/index.js
@@ -2,13 +2,3 @@
 export AccordionDelivery from './Accordions/Delivery/AccordionDelivery'
 export AccordionProductAlert from './Accordions/ProductAlert/AccordionProductAlert'
 
-// BUTTONS
-export DefaultButton from './Buttons/DefaultButton'
-export PrimaryButton from './Buttons/PrimaryButton'
-export SuccessButton from './Buttons/SuccessButton'
-export ClubOButton from './Buttons/ClubOButton'
-export GhostButton from './Buttons/GhostButton'
-
-// INPUTS
-export Textbox from './Inputs/Textbox'
-
